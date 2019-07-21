b6d1a0ff43918500229809bf3ea826b827000d2c
e <chengyingjuly@163.com>
2019-07-21 21:18:56 +0800

    adjust lr

--- vgg_face.py
+++ vgg_face.py
@@ -5,66 +5,70 @@ import keras.backend as K
 
 K.set_image_dim_ordering('tf')
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+import numpy as np
+import pandas as pd
+import os
+import gc
 
 from collections import defaultdict
 from glob import glob
 from random import choice, sample
-import math
-
+from keras.preprocessing import image
 import cv2
+from tqdm import tqdm_notebook
 import numpy as np
 import pandas as pd
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, LearningRateScheduler
-from keras.layers import Input, Dense, BatchNormalization, Activation, GlobalMaxPool2D, GlobalAvgPool2D, Concatenate, \
-    Multiply, Dropout, Subtract
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.layers import Input, Dense, GlobalMaxPool2D, GlobalAvgPool2D, Concatenate, Multiply, Dropout, Subtract
 from keras.models import Model
 from keras.optimizers import Adam
+
+
+
 from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
-from datetime import datetime
-from keras.callbacks import TensorBoard
-from keras.preprocessing import image
-import keras
-from keras.engine.topology import Layer
-from FMLayer import FMLayer
-from compact_bilinear_pooling import compact_bilinear_pooling_layer
 
-model_name = "kinship_{}".format(datetime.now().strftime('%b%d_%H-%M-%S'))
-tensor_board = TensorBoard(log_dir="../tb_log/" + model_name)
+train_file_path = "../input/train_relationships.csv"
+train_folders_path = "../input/train/"
 
-train_file_path = "input/train_relationships.csv"
-train_folders_path = "input/train/"
-val_famillies = "F09"
+# val_famillies_list = ["F01", "F02", "F03", "F04", "F05", "F06", "F07", "F08", "F09"]
+val_famillies_list = [ "F09"]
 
 all_images = glob(train_folders_path + "*/*/*.jpg")
-
-train_images = [x for x in all_images if val_famillies not in x]
-val_images = [x for x in all_images if val_famillies in x]
-
-train_person_to_images_map = defaultdict(list)
-
-ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
-
-for x in train_images:
-    train_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
-
-val_person_to_images_map = defaultdict(list)
-
-for x in val_images:
-    val_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
-
 relationships = pd.read_csv(train_file_path)
-relationships = list(zip(relationships.p1.values, relationships.p2.values))
-relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
 
-train = [x for x in relationships if val_famillies not in x[0]]
-val = [x for x in relationships if val_famillies in x[0]]
+
+def get_train_val(family_name, relationships=relationships):
+    # Get val_person_image_map
+    val_famillies = family_name
+    train_images = [x for x in all_images if val_famillies not in x]
+    val_images = [x for x in all_images if val_famillies in x]
+
+    train_person_to_images_map = defaultdict(list)
+
+    ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
+
+    for x in train_images:
+        train_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
+
+    val_person_to_images_map = defaultdict(list)
+
+    for x in val_images:
+        val_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
+
+    # Get the train and val dataset
+    #     relationships = pd.read_csv(train_file_path)
+    relationships = list(zip(relationships.p1.values, relationships.p2.values))
+    relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
+
+    train = [x for x in relationships if val_famillies not in x[0]]
+    val = [x for x in relationships if val_famillies in x[0]]
+
+    return train, val, train_person_to_images_map, val_person_to_images_map
 
 
 def read_img(path):
-    img = image.load_img(path, target_size=(197, 197))
-    # img = cv2.imread(path)
+    img = image.load_img(path, target_size=(224, 224))
     img = np.array(img).astype(np.float)
     return preprocess_input(img, version=2)
 
@@ -96,77 +100,40 @@ def gen(list_tuples, person_to_images_map, batch_size=16):
 
 
 def baseline_model():
-    input_1 = Input(shape=(197, 197, 3))
-    input_2 = Input(shape=(197, 197, 3))
+    input_1 = Input(shape=(224, 224, 3))
+    input_2 = Input(shape=(224, 224, 3))
 
-    # base_model = VGGFace(model='resnet50', include_top=False)
     base_model = VGGFace(model='resnet50', include_top=False)
 
     for x in base_model.layers[:-3]:
         x.trainable = True
-
-    # 一、 fix architecture bug
-    # base_model = Model(base_model.input, base_model.layers[-2].output)
+    for x in base_model.layers[-3:]:
+        x.trainable = False
 
     x1 = base_model(input_1)
     x2 = base_model(input_2)
 
-    # x1_ = Reshape(target_shape=(7*7, 2048))(x1)
-    # x2_ = Reshape(target_shape=(7*7, 2048))(x2)
-    #
-    # x_dot = Dot(axes=[2, 2], normalize=True)([x1_, x2_])
-    # x_dot = Flatten()(x_dot)
+    #     x1_ = Reshape(target_shape=(7*7, 2048))(x1)
+    #     x2_ = Reshape(target_shape=(7*7, 2048))(x2)
+    #     #
+    #     x_dot = Dot(axes=[2, 2], normalize=True)([x1_, x2_])
+    #     x_dot = Flatten()(x_dot)
 
-    # x = Subtract()([x1, x2])
-
-    # region
-
-    # region end
-    #
-    # x = keras.layers.Conv2D(1024, 1)(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.3)(x)
-
-    # x = keras.layers.Conv2D(1024, 3)(x)
-    # x = BatchNormalization()(x)
-    # x = Activation('relu')(x)
-    # x = Dropout(0.3)(x)
-
-    #
-    # x = keras.layers.Conv2D(2048, 3)(x)
-
-    # x = GlobalAvgPool2D()(x)
-    # x2 = GlobalMaxPool2D()(x)
-    # x = Concatenate(axis=-1)([x1, x2])
-
-    # x1 = GlobalAvgPool2D()(x1)
-    # x2 = GlobalAvgPool2D()(x2)
-    #
     x1 = Concatenate(axis=-1)([GlobalMaxPool2D()(x1), GlobalAvgPool2D()(x1)])
     x2 = Concatenate(axis=-1)([GlobalMaxPool2D()(x2), GlobalAvgPool2D()(x2)])
 
     x3 = Subtract()([x1, x2])
-
     x3 = Multiply()([x3, x3])
-    # x = x3
-    #
-    # # x = Multiply()([x1, x2])
-    # # x1**2-x2**2
+
     x1_ = Multiply()([x1, x1])
     x2_ = Multiply()([x2, x2])
-    x = Subtract()([x1_, x2_])
-    # # concat
-    x = Concatenate(axis=-1)([x, x3])
+    x4 = Subtract()([x1_, x2_])
+    x = Concatenate(axis=-1)([x4, x3])
 
-    # x = compact_bilinear_pooling_layer(x1.shape, x2.shape, 200, sum_pool=True)(x1, x2)
-
-    # x = Dropout(0.1)(x)
     x = Dense(100, activation="relu")(x)
-    # x = FMLayer(200, 100)(x)
-    x = Dropout(0.01)(x)
-    # x = Dense(50, activation="relu")(x)
-    # x = Dropout(0.01)(x)
+    x = Dropout(0.3)(x)
+    x = Dense(25, activation="relu")(x)
+    x = Dropout(0.3)(x)
     out = Dense(1, activation="sigmoid")(x)
 
     model = Model([input_1, input_2], out)
@@ -178,57 +145,29 @@ def baseline_model():
     return model
 
 
-file_path = "vgg_face.h5"
+model1 = baseline_model()
+n_val_famillies_list = len(val_famillies_list)
 
-checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-
-reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.1, patience=20, verbose=1)
-
-
-def step_decay(epoch):
-    initial_lrates = [0.00001, 0.000001, 0.0000001]
-
-    if epoch < 20:
-        return initial_lrates[0]
-    elif epoch < 60:
-        return initial_lrates[1]
-    else:
-        return initial_lrates[2]
+val_acc_list = []
 
 
-lrate = LearningRateScheduler(step_decay)
+def train_model1():
+    for i in tqdm_notebook(range(n_val_famillies_list)):
+        train, val, train_person_to_images_map, val_person_to_images_map = get_train_val(val_famillies_list[i])
+        file_path = f"vgg_face_{i}.h5"
+        checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.2, patience=5, verbose=1)
+        es = EarlyStopping(monitor="val_acc", min_delta=0.005, patience=8, verbose=1)
+        callbacks_list = [checkpoint, reduce_on_plateau, es]
 
-callbacks_list = [checkpoint, reduce_on_plateau, tensor_board]
-
-model = baseline_model()
-# model.load_weights(file_path)
-model.fit_generator(gen(train, train_person_to_images_map, batch_size=16), use_multiprocessing=True,
-                    validation_data=gen(val, val_person_to_images_map, batch_size=16), epochs=100, verbose=2,
-                    workers=4, callbacks=callbacks_list, steps_per_epoch=200, validation_steps=100)
-
-test_path = "input/test/"
+        history = model1.fit_generator(gen(train, train_person_to_images_map, batch_size=16),
+                                       use_multiprocessing=True,
+                                       validation_data=gen(val, val_person_to_images_map, batch_size=16),
+                                       epochs=120, verbose=1,
+                                       workers=4, callbacks=callbacks_list,
+                                       steps_per_epoch=240, validation_steps=120)
+        val_acc_list.append(np.max(history.history['val_acc']))
 
 
-def chunker(seq, size=32):
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
-
-
-from tqdm import tqdm
-
-submission = pd.read_csv('input/sample_submission.csv')
-
-predictions = []
-
-for batch in tqdm(chunker(submission.img_pair.values)):
-    X1 = [x.split("-")[0] for x in batch]
-    X1 = np.array([read_img(test_path + x) for x in X1])
-
-    X2 = [x.split("-")[1] for x in batch]
-    X2 = np.array([read_img(test_path + x) for x in X2])
-
-    pred = model.predict([X1, X2]).ravel().tolist()
-    predictions += pred
-
-submission['is_related'] = predictions
-
-submission.to_csv("vgg_face.csv", index=False)
+train_model1()
+print(val_acc_list)
